b49f688ae5a7b48581031e029b41ccbe793a3b2d
v_chumingyuan <saberrrc@163.com>
2017-12-27 19:51:16 +0800

    添加不崩溃crash处理

--- app/src/main/java/com/saberrrc/cmy/App.java
+++ app/src/main/java/com/saberrrc/cmy/App.java
@@ -1,11 +1,15 @@
 package com.saberrrc.cmy;
 
 import android.app.Application;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader;
 import com.bumptech.glide.load.model.GlideUrl;
 import com.saberrrc.cmy.common.buildconfig.AppBuildConfig;
+import com.saberrrc.cmy.common.crash.Cockroach;
 import com.saberrrc.cmy.common.net.https.ProvideOkhttpClientTrust;
 import com.saberrrc.cmy.di.component.AppComponent;
 import com.saberrrc.cmy.di.component.DaggerAppComponent;
@@ -36,6 +40,9 @@ public class App extends Application {
         initSDK();
         initAutoLayout();
         //initLeakCanary();
+        if (!DEBUG) {
+            initCrash();
+        }
     }
 
     private void initLeakCanary() {
@@ -61,5 +68,33 @@ public class App extends Application {
 
     private void initAppComponent() {
         appComponent = DaggerAppComponent.builder().appModule(new AppModule(this)).retrofitModule(new RetrofitModule(this)).build();
+    }
+
+    private void initCrash() {
+
+        Cockroach.install(new Cockroach.ExceptionHandler() {
+
+            // handlerException内部建议手动try{  你的异常处理逻辑  }catch(Throwable e){ } ，以防handlerException内部再次抛出异常，导致循环调用handlerException
+
+            @Override
+            public void handlerException(final Thread thread, final Throwable throwable) {
+                //开发时使用Cockroach可能不容易发现bug，所以建议开发阶段在handlerException中用Toast谈个提示框，
+                //由于handlerException可能运行在非ui线程中，Toast又需要在主线程，所以new了一个new Handler(Looper.getMainLooper())，
+                //所以千万不要在下面的run方法中执行耗时操作，因为run已经运行在了ui线程中。
+                //new Handler(Looper.getMainLooper())只是为了能弹出个toast，并无其他用途
+                new Handler(Looper.getMainLooper()).post(new Runnable() {
+                    @Override
+                    public void run() {
+                        try {
+                            //建议使用下面方式在控制台打印异常，这样就可以在Error级别看到红色log
+                            Log.e("AndroidRuntime", "--->CockroachException:" + thread + "<---", throwable);
+                            //                        throw new RuntimeException("..."+(i++));
+                        } catch (Throwable e) {
+
+                        }
+                    }
+                });
+            }
+        });
     }
 }
